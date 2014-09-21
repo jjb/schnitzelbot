abaaa8aa25ef3583b679b3821b158f81180e9e69
John Bachir <j@jjb.cc>
2014-09-20 21:09:53 -0400

    also favorite chicken-fried things?

--- bot.py
+++ bot.py
@@ -30,4 +30,4 @@ auth.set_access_token(CLIENT_TOKEN, CLIENT_SECRET)
 api = tweepy.API(auth)
 
 stream = tweepy.Stream(auth, l)
-stream.filter(track=['schnitzel', '#schnitzel', '#schnitzelbot'])
+stream.filter(track=['schnitzel', '#schnitzel', '#schnitzelbot', 'chicken fried', 'chicken-fried', 'chickenfried''])
